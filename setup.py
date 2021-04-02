eb725085e826f6df1d7992e838385a3c56cc3e11
srebughini <ste.rebu@gmail.com>
2021-04-02 17:01:03 +0200

    Improve handling of version and other pkg informations

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
 import setuptools
 
-setuptools.setup(
-    name="asali",
-    version="0.1",
-    author="ASALI",
-    author_email="ste.rebu@outlook.it",
-    description="Library to model chemical reactors",
-    url="https://github.com/srebughini/ASALIPY",
-    classifiers=["Programming Language :: Python :: 3", "Operating System :: OS Independent"],
-    package_dir={"asali": "src"},
-    packages=["asali.utils", "asali.plotters", "asali.reactors"],
-    include_package_data=True
-)
+setuptools.setup()
